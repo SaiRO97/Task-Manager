303b4334960b2353262f38ce9589eb3cecb6b6ca
ruslanotonbaev <otonbaevrus@gmail.com>
2020-02-11 15:50:11 +0600

    implement bcrypt for password

--- src/auth/auth.entity.ts
+++ src/auth/auth.entity.ts
@@ -11,6 +11,9 @@ export class User extends BaseEntity {
   username: string;
 
   @Column()
+  salt: string;
+
+  @Column()
   password: string;
 
 }
--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -3,6 +3,7 @@ import { User } from './auth.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { DUPLICATE_USERNAME_ERROR_CODE, USERNAME_ALREADY_EXIST_ERROR } from 'src/const/common';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -12,7 +13,8 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();;
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -23,6 +25,10 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 
 }
