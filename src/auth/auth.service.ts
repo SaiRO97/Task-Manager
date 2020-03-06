78a8f9ccbe6726ab95bda43597f63859f8bbf910
stonetiger1997@gmail.com <otonbaevrus@gmail.com>
2020-03-06 22:33:56 +0600

    added jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './auth.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { INVALID_CREDENTIALS } from 'src/const/common';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
-  ) {
-  }
+    private jwtService: JwtService
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(authCredentialsDto: AuthCredentialsDto):Promise<{accessToken:string}> {
     const username = await this.userRepository.validateUserPassword(authCredentialsDto);
     if (!username) {
       throw new UnauthorizedException(INVALID_CREDENTIALS);
     }
+
+    const payload: JwtPayload = { username }
+    const accessToken =  this.jwtService.sign(payload);
+
+    return {accessToken}
   }
 
 }
